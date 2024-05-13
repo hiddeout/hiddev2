b5f6a6c4df9f6a59b3f7fb1684341423aeb60b10
Junaid Javed <hiddeoutcollective@Gmail.com>
2024-05-12 21:08:44 -0400

    q

--- cogs/Moderation.py
+++ cogs/Moderation.py
@@ -61,18 +61,17 @@ class Moderation(commands.Cog):
             embed = discord.Embed(description=description, color=Colors.red)
             await ctx.send(embed=embed)
 
-    @commands.command(name="unban", help="Unban a member from the server", usage="[member]", description="Moderation")
-    @commands.cooldown(1, 10, commands.BucketType.guild)
+    @commands.command(name="unban")
     @commands.has_permissions(ban_members=True)
     async def unban(self, ctx, user: discord.User = None):
         """Unban a member from the server."""
         if user is None:
-            await self.create_embed(ctx, "Command: unban", "Unbans the mentioned user from the guild.\nSyntax & Example: ```Syntax: ,unban (user)\nExample: ,unban @omtfiji```", discord.Color.default())
+            await self.create_embed(ctx, "Command: unban", "Unbans the mentioned user from the guild.\nSyntax & Example: ```Syntax: ,unban (user)\nExample: ,unban @user```", discord.Color.default())
             return
-        
+    
         try:
             await ctx.guild.unban(user)
-            description = f"{Emojis.check} {ctx.author.mention} `{user}` has been unbanned."
+            description = f"{Emojis.check} `{user}` has been unbanned."
             embed = discord.Embed(description=description, color=Colors.green)
             await ctx.send(embed=embed)
         except discord.NotFound:
@@ -83,6 +82,143 @@ class Moderation(commands.Cog):
             description = f"{Emojis.wrong} Failed to unban `{user}`."
             embed = discord.Embed(description=description, color=Colors.red)
             await ctx.send(embed=embed)
+
+
+    @commands.command(name="massunban")
+    @commands.has_permissions(ban_members=True)
+    async def massunban(self, ctx):
+        """Unban all users from the server."""
+        ban_list = await ctx.guild.bans()
+        total_unbanned = 0
+        for ban_entry in ban_list:
+            user = ban_entry.user
+            try:
+                await ctx.guild.unban(user)
+                total_unbanned += 1
+            except discord.Forbidden:
+                await ctx.send(f"Failed to unban {user.name}#{user.discriminator}.")
+                continue
+            except Exception as e:
+                await ctx.send(f"An error occurred: {str(e)}")
+                continue
+    
+        embed = discord.Embed(
+            color=discord.Color.green(),
+            description=f"{Emojis.check} Successfully unbanned {total_unbanned} users."
+        )
+        await ctx.send(embed=embed)
+
+
+    @commands.command(name="mute")
+    @commands.has_permissions(manage_roles=True)
+    async def mute(self, ctx, member: discord.Member = None, duration: int = None, *, reason: str = "No reason provided"):
+        """Mute a member in the server."""
+        if member is None or duration is None:
+            await self.create_embed(ctx, "Command: mute", "Mutes the mentioned user in the server for a specified duration.\nSyntax & Example: ```Syntax: ,mute (user) (duration in minutes) [reason]\nExample: ,mute @user 10 Spamming```", discord.Color.default())
+            return
+    
+        if ctx.author == member:
+            return await ctx.send(f"{Emojis.wrong} You cannot mute yourself.")
+    
+        if member.top_role >= ctx.author.top_role and ctx.author != ctx.guild.owner:
+            return await ctx.send(f"{Emojis.wrong} You cannot mute this member due to role hierarchy.")
+    
+        try:
+            mute_role = discord.utils.get(ctx.guild.roles, name="Muted")
+            if not mute_role:
+                mute_role = await ctx.guild.create_role(name="Muted")
+            await member.add_roles(mute_role)
+            await ctx.send(embed=discord.Embed(description=f"{Emojis.check} {member.display_name} has been muted for {duration} minutes. Reason: {reason}", color=discord.Color.green()))
+            await asyncio.sleep(duration * 60)  # Converts minutes to seconds
+            await member.remove_roles(mute_role)
+            await ctx.send(f"{Emojis.check} {member.display_name} has been unmuted.")
+        except discord.Forbidden:
+            await ctx.send(embed=discord.Embed(description=f"{Emojis.wrong} I do not have permission to mute.", color=discord.Color.red()))
+        except Exception as e:
+            logging.error(f"Error muting member: {e}")
+            await ctx.send(embed=discord.Embed(description=f"{Emojis.wrong} An unexpected error occurred.", color=discord.Color.red()))
+
+    @commands.command(name="kick")
+    @commands.has_permissions(kick_members=True)
+    async def kick(self, ctx, member: discord.Member = None, *, reason: str = "No reason provided"):
+        """Kick a member from the server."""
+        if member is None:
+            await self.create_embed(ctx, "Command: kick", "Kicks the mentioned user from the server.\nSyntax & Example: ```Syntax: ,kick (user) [reason]\nExample: ,kick @user Breaking rules```", discord.Color.default())
+            return
+    
+        if ctx.author == member:
+            return await ctx.send(f"{Emojis.wrong} You cannot kick yourself.")
+    
+        if member.top_role >= ctx.author.top_role and ctx.author != ctx.guild.owner:
+            return await ctx.send(f"{Emojis.wrong} You cannot kick this member due to role hierarchy.")
+    
+        try:
+            await member.kick(reason=reason)
+            description = f"{Emojis.check} {member.display_name} has been kicked. Reason: {reason}"
+            embed = discord.Embed(description=description, color=Colors.green)
+            await ctx.send(embed=embed)
+        except discord.Forbidden:
+            description = f"{Emojis.wrong} I do not have permission to kick."
+            embed = discord.Embed(description=description, color=Colors.red)
+            await ctx.send(embed=embed)
+        except Exception as e:
+            logging.error(f"Error kicking member: {e}")
+            description = f"{Emojis.wrong} An unexpected error occurred."
+            embed = discord.Embed(description=description, color=Colors.red)
+            await ctx.send(embed=embed)
+   
+    @commands.group(invoke_without_command=True)
+    @commands.has_permissions(manage_channels=True)
+    async def lock(self, ctx, channel: discord.TextChannel = None, *, reason="No reason provided"):
+        """Lock a specified channel or the current channel if no channel is specified."""
+        if channel is None:
+            channel = ctx.channel  # Default to the current channel if none is specified
+    
+        await channel.set_permissions(ctx.guild.default_role, send_messages=False, reason=reason)
+        embed = discord.Embed(
+            color=discord.Color.green()  # Adjust the color to match your bot's theme
+        )
+        embed.description = f"{Emojis.check} {ctx.guild.me.mention}: {channel.mention} is now locked down for @everyone."
+        await ctx.send(embed=embed)
+    
+    @lock.command(name="all")
+    async def lock_all(self, ctx, *, reason="No reason provided"):
+        """Lock all channels in the guild."""
+        for channel in ctx.guild.text_channels:
+            await channel.set_permissions(ctx.guild.default_role, send_messages=False, reason=reason)
+        embed = discord.Embed(
+            color=discord.Color.green()  # Adjust the color to match your bot's theme
+        )
+        embed.description = f"{Emojis.check} {ctx.guild.me.mention} All channels have been locked down for @everyone."
+        await ctx.send(embed=embed)
+    
+    @commands.group(invoke_without_command=True)
+    @commands.has_permissions(manage_channels=True)
+    async def unlock(self, ctx, channel: discord.TextChannel = None, *, reason="No reason provided"):
+        """Unlock a specified channel or the current channel if no channel is specified."""
+        if channel is None:
+            channel = ctx.channel  # Default to the current channel if none is specified
+    
+        await channel.set_permissions(ctx.guild.default_role, send_messages=True, reason=reason)
+        embed = discord.Embed(
+            color=discord.Color.green()  # Adjust the color to match your bot's theme
+        )
+        embed.description = f"{Emojis.check} {ctx.guild.me.mention}: {channel.mention} has been unlocked for @everyone."
+        await ctx.send(embed=embed)
+    
+    @unlock.command(name="all")
+    async def unlock_all(self, ctx, *, reason="No reason provided"):
+        """Unlock all channels in the guild."""
+        for channel in ctx.guild.text_channels:
+            await channel.set_permissions(ctx.guild.default_role, send_messages=True, reason=reason)
+        embed = discord.Embed(
+            color=discord.Color.green()  # Adjust the color to match your bot's theme
+        )
+        embed.description = f"{Emojis.check} {ctx.guild.me.mention} All channels have been unlocked for @everyone."
+        await ctx.send(embed=embed)
+    
+
+
 
 async def setup(bot):
     await bot.add_cog(Moderation(bot))
